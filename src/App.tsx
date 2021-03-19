faade159b5b50ab99b582581189e0d66610588b7
Radek <radek.padurski94@gmail.com>
2021-03-19 13:33:19 +0100

    task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import LoginForm from './LoginForm';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
